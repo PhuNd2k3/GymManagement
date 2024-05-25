2f9f54d9aadccc1e8cbbb8aafe7873e87d02070b
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-25 18:31:35 +0700

    mapper

--- Back-End/src/main/java/com/example/gymmanagement/converter/MembershipConverter.java
+++ Back-End/src/main/java/com/example/gymmanagement/converter/MembershipConverter.java
@@ -3,16 +3,17 @@ package com.example.gymmanagement.converter;
 
 import com.example.gymmanagement.dto.MembershipDTO;
 import com.example.gymmanagement.entity.Membership;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MembershipConverter {
+    @Autowired
+    private ModelMapper modelMapper;
     public MembershipDTO toMembershipDTO(Membership membership){
-        MembershipDTO membershipDTO = new MembershipDTO();
-        membershipDTO.setName(membership.getName());
-        membershipDTO.setPrice(membership.getPrice());
+        MembershipDTO membershipDTO = modelMapper.map(membership,MembershipDTO.class);
         membershipDTO.setMemberCount(membership.getMembers().size());
-        membershipDTO.setNumbersOfTrainingPerWeek(membership.getNumberOfTrainingPerWeek());
         return membershipDTO;
     }
 }
