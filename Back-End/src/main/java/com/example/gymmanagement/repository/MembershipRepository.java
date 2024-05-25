a6eb36559619be5833faae3e1052114e759996f2
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2024-05-25 15:38:45 +0700

    iie

--- Back-End/src/main/java/com/example/gymmanagement/repository/MembershipRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.gymmanagement.repository;
-
-import com.example.gymmanagement.entity.Membership;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MembershipRepository extends JpaRepository<Membership, Integer> {
-}
