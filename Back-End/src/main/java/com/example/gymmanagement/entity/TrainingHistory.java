6c20232d34ec4e4c7dcbbc91d9d98af807b89897
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-26 00:59:21 +0700

    update

---
+++ Back-End/src/main/java/com/example/gymmanagement/entity/TrainingHistory.java
@@ -0,0 +1,21 @@
+//package com.example.gymmanagement.entity;
+//
+//import jakarta.persistence.*;
+//import lombok.Data;
+//
+//import java.util.Date;
+//
+//@Entity
+//@Data
+//@Table(name = "training_history")
+//public class TrainingHistory {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Integer id;
+//
+//    @Column(name = "training_time")
+//    private Date trainingTime;
+//
+//    @ManyToOne
+//    @JoinColumn()
+//}
