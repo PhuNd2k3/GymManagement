080fa4aca5ff80bd30f27b1ef06af6a97e0b2392
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-26 11:57:41 +0700

    fix lại request

--- Back-End/src/main/java/com/example/gymmanagement/dto/request/SignUpMembershipRequest.java
+++ Back-End/src/main/java/com/example/gymmanagement/dto/request/SignUpMembershipRequest.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class SignUpMembershipRequest {
-    private Long memberId;
-    private Long membershipId;
+    private Integer memberId;
+    private Integer membershipId;
     private String payment_method;
 }
