c60b87f8f3a0c12610db89b38d566cbbb0c081cf
Chu Đình Hiển <hienc1212@gmail.com>
2024-05-27 00:58:30 +0700

    Fix profile + thêm trường

--- Back-End/src/main/java/com/example/gymmanagement/dto/RegisterMembershipDTO.java
+++ Back-End/src/main/java/com/example/gymmanagement/dto/RegisterMembershipDTO.java
@@ -14,4 +14,5 @@ public class RegisterMembershipDTO {
     private String phoneNumber;
     private Date dob;
     private Integer memberId;
+    private String paymentMethod;
 }
--- Back-End/src/main/java/com/example/gymmanagement/entity/Admin.java
+++ Back-End/src/main/java/com/example/gymmanagement/entity/Admin.java
@@ -2,11 +2,13 @@ package com.example.gymmanagement.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Entity
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Table(name = "admin")
 public class  Admin extends User{
     @OneToMany(mappedBy = "admin",fetch = FetchType.LAZY)
